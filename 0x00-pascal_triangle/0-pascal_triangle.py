547107c826b9c2109e65b1848cf2234655f4bd1f
fatma soliman <fatmacivil@hotmail.com>
2024-06-27 04:58:40 +0300

    pascal task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Pascal’s triangle'''
+
+
+def pascal_triangle(n):
+    '''Returns a list of lists of integers representing the Pascal’s triangle of n.'''
+    
+    if (n <= 0):
+        return []
+    
+    pascal = [[1]]
+    for i in range(n - 1):
+        row = [1]
+        for j in range(len(pascal[i]) - 1):
+            row.append(pascal[i][j] + pascal[i][j + 1])
+        row.append(1)
+        pascal.append(row)
+    return pascal
