a6484d89c13e3221b4648f7163a798e10a2ef12d
fatma soliman <fatmacivil@hotmail.com>
2024-07-25 21:09:39 +0300

    fx

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,22 +3,30 @@
 
 
 def validUTF8(data):
-    """determines if a given data set represents a valid UTF-8 encoding"""
-    if not data:
-        return False
-    char = 0
+    """Check if the data is a valid UTF-8 encoding"""
+    num_bytes = 0
+
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
     for byte in data:
-        if char:
-            if byte >> 6 != 0b10:
+        byte = byte & 0xFF
+        
+        if num_bytes == 0:
+            mask = 1 << 7
+            while mask & byte:
+                num_bytes += 1
+                mask = mask >> 1
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
                 return False
-            char -= 1
-            continue
-        if byte >> 7 == 0:
-            continue
-        while byte:
-            byte >>= 1
-            char += 1
-        if char == 1 or char > 4:
-            return False
-        char = max(char - 1, 0)
-    return char == 0
+        else:
+            
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+        
+        num_bytes -= 1
+
+    return num_bytes == 0
