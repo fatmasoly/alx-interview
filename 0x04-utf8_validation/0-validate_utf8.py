c7c30218f0f6906bc4e2ce4ac9b0ab3ffd5e5b14
fatma soliman <fatmacivil@hotmail.com>
2024-07-25 21:11:03 +0300

    fx style

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,7 +11,7 @@ def validUTF8(data):
 
     for byte in data:
         byte = byte & 0xFF
-        
+
         if num_bytes == 0:
             mask = 1 << 7
             while mask & byte:
@@ -23,10 +23,10 @@ def validUTF8(data):
             if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            
+
             if not (byte & mask1 and not (byte & mask2)):
                 return False
-        
+
         num_bytes -= 1
 
     return num_bytes == 0
